40c68734bf092e6b57d03c178af9f9be1f410e6a
Roy Lyons <Roy.Lyons@cmegroup.com>
2011-01-04 12:04:18 -0600

    needed cleanup

--- clearcase_ucm_import/gitimport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh -x 
-BASEDIR=`dirname $0`
-export PATH=/home/e15580/ActivePerl-5.12/site/bin:/home/e15580/ActivePerl-5.12/bin:$PATH
-export PERL5LIB=$BASEDIR/scripts
-perl $DEBUG $BASEDIR/scripts/cc2git.pl $@
